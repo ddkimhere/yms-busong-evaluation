ba39c3fa89def4111dd2ad752a8b2ccf17edfb3e
ddkimhere <ddkim1984@gmail.com>
2026-08-17 01:58:57 +0900

    Auto-connect Monthly Evaluation to Firebase

--- app-ui.js
+++ app-ui.js
@@ -3,4 +3,14 @@ renderSubjects();
 renderScores();
 el('evaluationYear').value=new Date().getFullYear();
 el('evaluationMonth').value=`${new Date().getMonth()+1}월`;
+
+const YMS_FIREBASE_CONFIG={
+  apiKey:'AIzaSyBuXxBX-BffkDQBYL0bDIaQLMkBUC0B3f8',
+  authDomain:'yms-ele-evaluation.firebaseapp.com',
+  projectId:'yms-ele-evaluation',
+  storageBucket:'yms-ele-evaluation.firebasestorage.app',
+  messagingSenderId:'578400571836',
+  appId:'1:578400571836:web:82ddacfb46f6b52e419bb9'
+};
+localStorage.setItem('yms_evaluation_firebase_config',JSON.stringify(YMS_FIREBASE_CONFIG));
 initFirebaseFromSaved();
